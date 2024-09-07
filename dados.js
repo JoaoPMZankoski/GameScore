2998b5b51c6cc123bf5bde5170921d1ec0f0a290
JoaoPMZankoski <joaopzankoski@gmail.com>
2024-09-07 15:28:41 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Baldur's Gate 3",
+        descricao: "Baldur's Gate 3 é um RPG de nova geração que mergulha os jogadores no rico universo de Dungeons & Dragons.",
+        link: "https://store.steampowered.com/app/1086940/Baldurs_Gate_3/",
+        tags: "rpg  crpg turno fantasia"
+    },
+    {
+        titulo: "Black Myth: Wukong",
+        descricao: "Uma aventura épica inspirada na lenda chinesa do Rei Macaco, com visuais impressionantes e combate frenético.",
+        link: "https://store.steampowered.com/app/2358720/Black_Myth_Wukong/",
+        tags: "mitologia macaco rpg soulslike"
+      },
+      {
+        titulo: "Red Dead Redemption 2",
+        descricao: "Um épico western de mundo aberto que narra a história de Arthur Morgan e da gangue de Van der Linde em decadência.",
+        link: "https://store.steampowered.com/app/1174180/Red_Dead_Redemption_2/",
+        tags: "rpg velho oeste mundo aberto"
+      },
+
+];
